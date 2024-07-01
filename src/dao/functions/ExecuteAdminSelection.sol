44696ebe74f1ccda1475fabac09317f84b1c61cb
shogochiai <shogo.ochiai@protonmail.com>
2024-07-01 17:28:41 +0900

    min-heap sort for admin selection

--- src/dao/functions/ExecuteAdminSelection.sol
+++ src/dao/functions/ExecuteAdminSelection.sol
@@ -10,48 +10,63 @@ contract ExecuteAdminSelection {
         require(!s.users[msg.sender].isBanned, "User is banned");
 
         uint256 totalStake = 0;
-        address[] memory candidates;
-        uint256 candidateCount = 0;
+        address[] memory minHeap = new address[](55);  // Min-heap of size sqrt(3000) ≈ 55
+        uint256 heapSize = 0;
 
         // Collect active users with stakes
         for (uint256 i = 0; i < s.userList.length; i++) {
             address userAddress = s.userList[i];
             if (s.users[userAddress].isActive && s.users[userAddress].stakeAmount > 0) {
                 totalStake += s.users[userAddress].stakeAmount;
-                candidates[candidateCount] = userAddress;
-                candidateCount++;
-            }
-        }
 
-        // Sort candidates by stake amount (in descending order)
-        for (uint256 i = 0; i < candidateCount; i++) {
-            for (uint256 j = i + 1; j < candidateCount; j++) {
-                if (s.users[candidates[i]].stakeAmount < s.users[candidates[j]].stakeAmount) {
-                    address temp = candidates[i];
-                    candidates[i] = candidates[j];
-                    candidates[j] = temp;
+                // Maintain min-heap of top 55 staked users
+                if (heapSize < 55) {
+                    minHeap[heapSize] = userAddress;
+                    heapSize++;
+                    if (heapSize == 55) {
+                        buildMinHeap(minHeap, s);
+                    }
+                } else if (s.users[userAddress].stakeAmount > s.users[minHeap[0]].stakeAmount) {
+                    minHeap[0] = userAddress;
+                    minHeapify(minHeap, 0, s);
                 }
             }
         }
 
-        // Select top candidates as admins
-        uint256 adminCount = uint256(sqrt(candidateCount));
-        for (uint256 i = 0; i < adminCount; i++) {
-            s.admins[candidates[i]].userID = candidates[i];
-            s.admins[candidates[i]].stakeAmount = s.users[candidates[i]].stakeAmount;
+        // Update admins with the addresses in minHeap
+        for (uint256 i = 0; i < heapSize; i++) {
+            s.admins[minHeap[i]].userID = minHeap[i];
+            s.admins[minHeap[i]].stakeAmount = s.users[minHeap[i]].stakeAmount;
         }
 
         emit AdminSelectionExecuted();
     }
 
-    function sqrt(uint256 x) private pure returns (uint256) {
-        uint256 z = (x + 1) / 2;
-        uint256 y = x;
-        while (z < y) {
-            y = z;
-            z = (x / z + z) / 2;
+    function buildMinHeap(address[] memory heap, Schema.GlobalState storage s) private {
+        for (uint256 i = heap.length / 2; i > 0; i--) {
+            minHeapify(heap, i - 1, s);
         }
-        return y;
+    }
+
+    function minHeapify(address[] memory heap, uint256 i, Schema.GlobalState storage s) private {
+        uint256 left = 2 * i + 1;
+        uint256 right = 2 * i + 2;
+        uint256 smallest = i;
+
+        if (left < heap.length && s.users[heap[left]].stakeAmount < s.users[heap[smallest]].stakeAmount) {
+            smallest = left;
+        }
+
+        if (right < heap.length && s.users[heap[right]].stakeAmount < s.users[heap[smallest]].stakeAmount) {
+            smallest = right;
+        }
+
+        if (smallest != i) {
+            address temp = heap[i];
+            heap[i] = heap[smallest];
+            heap[smallest] = temp;
+            minHeapify(heap, smallest, s);
+        }
     }
 
     event AdminSelectionExecuted();
